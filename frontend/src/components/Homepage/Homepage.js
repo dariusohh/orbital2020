53d920e8cf167c3e7146da1a8ec04f2e735fb2d4
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-05-31 15:24:52 +0800

    zeng(2)

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {Grid,Cell} from 'react-mdl';
 import './home.css';
-import { Container, Row, Col } from 'react-grid-system';
-import Image from './assets/Image.jpg';
-import {Link } from "react-router-dom";
 import Spacer from 'react-add-space';
+import {Container}from 'react-grid-system';
+import i from './assets/i.jpg';
+import {Link } from "react-router-dom";
+import A from './assets/A.jpg';
+import B from './assets/B.jpg';
+
+
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+
 
 
 class Homepage extends React.Component {
@@ -15,44 +19,86 @@ class Homepage extends React.Component {
 
   render() {
   return (
-      
-    <div style= {{width:'100%', margin:'auto'}}>
-    <Grid className="homepage-grid"> 
-        <Cell col= {12}>
-        <img 
-        src= {Image}
+
+<div> 
+<Container className='containers'> </Container>
+<img 
+        src= {i}
         alt ="avater"
         className= "avatar-img"
         />
-        <Spacer amount={50} />
-    <Container  >
-    <Row>
-        <Col sm={5} className = 'containers'>
-         <h1> Venture Capitalist </h1>
-         <button className="btn">
-              click to check out the features
-            </button>
-        </Col>
-        <Col sm={2} >
-        </Col>
 
-        <Col sm={5} className = 'containers'>
-        <h1>  Start-ups    
-        </h1>
+<Container className='space'>
+
+</Container>
+
+<h1 className ='banner'>
+  AnalyticsWallet Welcomes You 
+</h1>
+
+  <p className='text'>“ we are the first e-wallet web application designed 
+    to help start-up companies keep track of their finances 
+    as well as to connect with Venture Capitalists.”
+</p>
+
+
+<Container className='space2'></Container>
+
+<Container className='c'>
+<Link to="/Register">
+            <button className="Button">
+              click to start your journey with us
+            </button>
+            </Link>
+</Container>
+
+<Container className='space'></Container>
+
+<h1 className ='banner2'>
+    Our Services
+</h1>
+<Container className='space'></Container>
+
+
+  
+<div class="wrapper">
+  <div className= 'Su'>
+    <img 
+        src= {B}
+        className='B'
+        />
+        <Spacer amount={2} />
+        <h1 className='VCtext'> Venture-Capitalists </h1>
+        <Container>
         <Link to="/Login">
-            <button className="btn">
+            <button className="Button">
               click to check out the features
             </button>
             </Link>
-        </Col>
-    </Row>
-</Container>
+            </Container>
+</div>
+  <div className='Su'>
+  <img 
+        src= {A}
+        className='B'
+        />
+<Spacer amount={2} />
+  <h1 className='VCtext'> Start-Up </h1> 
+  <Container >
+  <Link to="/Login">
+            <button className="Button">
+              click to check out the features
+            </button>
+            </Link>
+  </Container>
+  
+  </div>
+  
+  </div>
+
+</div>
 
 
-
-        </Cell>
-        </Grid>
-    </div>
     )
   }
 }
@@ -64,5 +110,8 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(Homepage);
+
+
+
 
 

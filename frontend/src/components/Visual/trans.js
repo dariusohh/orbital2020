1d70eada4302705c020830e82a0a7e56c38c34ff
Darius Oh <ohsterx@gmail.com>
2020-06-07 22:52:10 +0800

    Fix visual

--- frontend/src/components/Visual/trans.js
+++ frontend/src/components/Visual/trans.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Expense} from '../Expense/Expense';
-import {Revenue} from '../Expnese/Revenue';
+import {Revenue} from '../Expense/Revenue';
 import {Link } from "react-router-dom";
 import axios from 'axios';
 
@@ -17,6 +17,9 @@ class Trans extends React.Component {
         <ul className="list">
           {this.props.data.slice(-4, -1).map(transaction => (
           <Expense key={transaction.id} transaction={transaction} />
+          ))}
+          {this.props.data.slice(-4, -1).map(transaction => (
+          <Revenue key={transaction.id} transaction={transaction} />
           ))}
         </ul>
     </>

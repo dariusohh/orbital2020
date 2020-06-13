a85580fcccab5d492c0bcc8b31ba21bccd4d1883
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-13 17:36:43 +0800

    Update serializers.py
    add ratings and contact details

--- backend/userprofile/serializers.py
+++ backend/userprofile/serializers.py
@@ -4,4 +4,5 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["username","company_name","company_industry","company_description","show_public"]
+        fields = ["username","company_name","company_industry","company_description","show_public"
+                   ,"email","office","tele","ratings"]

528bd5dd7e619c90f93fa2ca766d0f7c36ff29b9
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-13 17:36:05 +0800

    Update models.py
    add ratings and contact details

--- backend/userprofile/models.py
+++ backend/userprofile/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    username = models.TextField(unique = True, db_index = True,primary_key = True);
-    company_name = models.TextField(blank = True);
-    company_industry = models.TextField(blank = True);
-    company_description = models.TextField(blank = True);
-    show_public = models.BooleanField();
+    username = models.TextField(unique = True, db_index = True,primary_key = True)
+    company_name = models.TextField(blank = True)
+    company_industry = models.TextField(blank = True)
+    company_description = models.TextField(blank = True)
+    show_public = models.BooleanField()
+     email = models.TextField(blank = True)
+    office = models.TextField(blank = True)
+    tele = models.TextField(blank = True)
+    ratings = models.IntegerField(blank=True)
+
+   
+  

02349dfdc2f7268ca5031c7a2e29db7b2eb827ae
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-15 00:35:27 +0800

    Update serializers.py

--- backend/userprofile/serializers.py
+++ backend/userprofile/serializers.py
@@ -4,5 +4,4 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["username","company_name","company_industry","company_description","show_public"
-                   ,"email","office","tele","ratings","budget","target"]
+        fields = '__all__'

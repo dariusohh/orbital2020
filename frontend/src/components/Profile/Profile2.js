f1aa2dedefb8511b64a4a5895b89eef0f52f2210
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-15 22:11:58 +0800

    Update Profile2.js
     change default display

--- frontend/src/components/Profile/Profile2.js
+++ frontend/src/components/Profile/Profile2.js
@@ -23,7 +23,7 @@ class Profile2 extends React.Component {
         company_industry: "",
         company_description: "", 
         show_public: ""},
-        header:'Click to find out more about the company!',
+        header:'About Us',
         expense: [],
     update: false
     }
@@ -40,6 +40,7 @@ componentDidMount() {
       .then(prof => {
           this.setState({
               profile: prof.data,
+            text: <About data={prof.data}/>
           });
       })
       axios.get(`api/`)

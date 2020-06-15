f2a04372f4a2dcbdf78c40e178f7af77bb3791f4
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-15 17:48:29 +0800

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,11 +3,20 @@ from django.urls import path, include
 from expense.views import ExpenseViewSet
 from userprofile.views import UserProfileViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
+from post.views import PostView
+from django.urls import path, include
+
 
 router = DefaultRouter()
 router.register(r'api',ExpenseViewSet)
 router.register(r'profile',UserProfileViewSet)
+router.register(r'image',PostView)
 urlpatterns = router.urls
 urlpatterns += [path('rest_auth/', include('rest_auth.urls')),
                 path('rest_auth/register/',include('rest_auth.registration.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

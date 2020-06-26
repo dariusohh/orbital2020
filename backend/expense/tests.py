4e128ef48bcd66a8a4d147f2235a7137ea79c183
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-06-27 00:43:17 +0800

    Unit Testing

--- backend/expense/tests.py
+++ backend/expense/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from expense.models import Expense
 
-# Create your tests here.
+class TestModels(TestCase):
+    def setUp(self):
+        Expense.objects.create( username='test-1',
+            name='category-1',
+            amount=-10000)
+        Expense.objects.create( username='test-2',
+            name='category-2',
+            amount=10000)
+
+
+    def test_is_expense(self):
+        self.assertEquals(Expense.objects.get(username="test-1").is_expense(),True)
+        self.assertEquals(Expense.objects.get(username="test-2").is_expense(),False)
+
+    def test_is_revenue(self):
+        self.assertEquals(Expense.objects.get(username="test-1").is_revenue(),False)
+        self.assertEquals(Expense.objects.get(username="test-2").is_revenue(),True)
+
+
+    def test_is_name_valid(self):
+        self.assertEquals(Expense.objects.get(username="test-1").is_valid_name(),True)
+        self.assertEquals(Expense.objects.get(username="test-2").is_valid_name(),True)

aaa8906750cb23424122e9eb85baf0ee94fcbece
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:55:12 +0800

    Update add.js

--- frontend/src/components/Visual/add.js
+++ frontend/src/components/Visual/add.js
@@ -9,28 +9,14 @@ export class AddBudget extends React.Component {
       
         <>
            
-           <span style= {{fontSize:60, color:'IndianRed', textAlign:"center"}}>
-           $ {this.props.budget -(this.props.data
+           <>
+           ${this.props.budget -(this.props.data
           .map(transaction => transaction.amount)
           .map(num => parseFloat(num))
           .filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1)
           .toFixed(2)}
-          </span>
-          {/* <Spacer amount={5} />
-        <Popup trigger={<button className="Btn">Add Budget</button>} modal closeOnDocumentClick>
-        <div>
-            <h3 className='text-3'> Add Budget</h3>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-control">
-              <label htmlFor="amount"> Budget 
-                </label>
-              <input name= "amount" type="number"
-              placeholder="Enter amount..." />
-            </div>
-            <button className="btn">Add Budget</button>
-          </form>
-        </div>
-        </Popup> */}
+          </>
+   
         </>
 
     )

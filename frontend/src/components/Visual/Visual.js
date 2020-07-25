54bf0132889777e71bcf63361f6037fd42544e3e
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:54:32 +0800

    Update Visual.js

--- frontend/src/components/Visual/Visual.js
+++ frontend/src/components/Visual/Visual.js
@@ -1,26 +1,39 @@
 
 import axios from 'axios';
-import Revenue from './revenue';
 import React from 'react';
-import {BarGraph} from './bar';
-import Target from './target';
-import Trans from './trans';
-import './visual.css'
-import Spacer from 'react-add-space';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import './visual.css';
+import MoneyIcon from '@material-ui/icons/Money';
 import {BarGraph2} from './barR';
 import {Graph} from'./line';
-import {Row,Col}from 'react-grid-system';
+import TrackChangesIcon from '@material-ui/icons/TrackChanges';
 import {AddBudget} from './add';
-import {Time} from './timeline';
+import {BarGraph} from './bar';
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import AccessTime from "@material-ui/icons/AccessTime";
+import {Row,Col}from 'react-grid-system';
 import { connect } from 'react-redux';
 import { DateRangePicker } from 'rsuite';
-import 'rsuite/dist/styles/rsuite-default.css'
-
-import{StockOutlined,DollarCircleOutlined,UpCircleFilled } from  '@ant-design/icons';
-
+import Card from "../Card/Card.js";
+import Accessibility from "@material-ui/icons/Accessibility";
+import GridItem from "../Grid/GridItem";
+import GridContainer from "../Grid/GridContainer.js";
+import CardHeader from "../Card/CardHeader.js";
+import CardIcon from "../Card/CardIcon.js";
+import DateRange from "@material-ui/icons/DateRange";
+import Store from "@material-ui/icons/Store";
+import Update from "@material-ui/icons/Update";
+import CardFooter from "../Card/CardFooter.js";
+import Target from './target';
+import Revenue from './revenue';
+import 'rsuite/dist/styles/rsuite-default.css';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import CardBody from "../Card/CardBody.js";
+import dashboardStyle from '../assets/jss/material-dashboard-react/views/dashboardStyle.js';
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
-
+const useStyles = makeStyles(dashboardStyle);
 class Visualisation extends React.Component {
    
   state = {
@@ -29,8 +42,10 @@ class Visualisation extends React.Component {
     revenue:0,
     startDate: new Date(new Date().setDate(1)),
     endDate: new Date(),
-    filter:false
+    filter:false,
+    length:0
 }
+
 
 dateFilter = (objdate) => {
   const objyear = objdate.substring(0,4)
@@ -51,7 +66,8 @@ componentDidMount() {
       .then(res => {
           const new_res = res.data.filter(x => x.username === localStorage.getItem("username")).filter(x => this.dateFilter(x.created_at));
           this.setState({
-              expense: new_res
+              expense: new_res,
+              length: new_res.filter(x => x.amount >0).length
           });
       axios.get(`profile/${localStorage.getItem("username")}/`)
       .then(res => {
@@ -78,90 +94,185 @@ componentDidMount() {
   }
 
   render() {
-
+  
   return (
    <>
-   <h1 className='text'>Overall Analysis</h1>
-
-   <h2 className='line'> </h2>
-
-   { this.state.filter ? 
-        <button className= "filter-button" onClick={() => this.setState({filter:false})}>▼  Filter</button>
-            :
-        <button className= "filter-button" onClick={() => this.setState({filter:true})}>▶  Filter</button>
+     <Row>
+        <Col sm={10} >
+       <h3>OVERALL ANALYSIS</h3>
+        </Col>
+        <Col sm={2}>
+        { this.state.filter ? 
+        <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={() => this.setState({filter:false})}
+      
+      >
+        ▼  Filter Date
+        </Button>
+        :
+        <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={() => this.setState({filter:true})}
+      
+      >
+        ▶   Filter Date
+        </Button>
+        // <button className= "filter-button" onClick={() => this.setState({filter:false})}>▼  Filter</button>
+        //     :
+        // <button className= "filter-button" onClick={() => this.setState({filter:true})}>▶  Filter</button>
         }
-  {this.state.filter && 
-   <div>
-        <label className="date-label">Date:</label>
-      <DateRangePicker 
+        { this.state.filter && 
+          <div>
+        
+        <DateRangePicker 
         value={[this.state.startDate,this.state.endDate]}
         onChange={arr => this.setState({startDate:arr[0],endDate:arr[1]})}
         disabledDate={date => date.getTime() - new Date().getTime() > 0}/>
         </div>
-  }
-        <div className = "space-2"></div>
-   <div>
-   <Row>
-    <Col className='header1'><h2 className='text2'>Profit Target</h2>
-    <UpCircleFilled style={{ fontSize: '80px',color: '#08c' }}/>
-    <Spacer amount={10} />
-    <Target data={this.state.expense} target= {this.state.target}/>
-    </Col>
-    <Spacer amount={10} />
-    <Col className='header3'>
-    <h2 className='text2'>Nett Profit (SGD)</h2>
-    <StockOutlined style={{ fontSize: '90px',color: '#08c' }}/>
-    <Spacer amount={10} />
-    <Revenue data={this.state.expense}/>
-  
+        }
+        </Col>
+      </Row>
+      <br/>    <br/>   
+      <div>
+      <GridContainer>
+      <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <TrackChangesIcon style={{fontSize:'30px',color:'white'}}/>
+              </CardIcon>
+              <p style={{color:'black'}}>Profit Target</p>
+              <h3 style={{color:'black'}}>
+              <Target data={this.state.expense} target= {this.state.target}/><small>%</small>
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div >
+                <DateRange />
+                Last 24 Hours
+              </div>
+            </CardFooter>
+          </Card>
+       
+       
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <MoneyIcon/>
+              </CardIcon>
+              <p style={{color:'black'}} >Revenue</p>
+              <h3 style={{color:'black'}} ><Revenue data={this.state.expense}/></h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div >
+                <DateRange />
+                Last 24 Hours
+              </div>
 
-    </Col>
-    
-    <Spacer amount={10} />
-    <Col className='header4'><h2 className='text2'>Budget Left (SGD)</h2>
-    <DollarCircleOutlined  style={{ fontSize: '90px',color: '#08c' }}/>
-    <Spacer amount={8} />
-    <AddBudget data={this.state.expense} budget ={this.state.budget}/>
-    </Col>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <AccountBalanceIcon style={{fontSize:'30px',color:'white'}}/>
+              </CardIcon>
+              <p style={{color:'black'}} >Budget Left</p>
+              <h3 style={{color:'black'}} >  <AddBudget data={this.state.expense} budget ={this.state.budget}/></h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div >
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p style={{color:'black'}}>Consumer Count</p>
+              <h3 style={{color:'black'}}>{this.state.length}</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div >
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
 
-  </Row>
- 
-  </div>
- 
- <br/>
-<div className='space-1'></div>
-  <div>
-  <Row style = {{textAlign:"center", justifyContent:"center", display:"flex",flexWrap:"nowrap"}}>
-  <div style = {{width:"33%"}}>
-  <Col className='data' > <Graph data={this.state.expense} 
+
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader  >
+            <BarGraph data={this.state.expense} />
+            </CardHeader>
+            <CardBody>
+             
+              <p > Analysis of Company's Expenses               
+              </p>
+            </CardBody>
+            <CardFooter chart>
+              <div >
+                <AccessTime />  according to filtered dates
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader >
+            <Graph data={this.state.expense} 
                                   startDate = {this.state.startDate} 
                                   endDate = {this.state.endDate}
-                                  /> </Col>
-                                  </div>
-  <Spacer amount={2} />
-  <div style = {{width:"33%"}}>
-    <Col className='data' ><BarGraph data={this.state.expense} /></Col>
-    </div>
-    <Spacer amount={2} />
-    <div style = {{width:"33%"}}>
-    <Col className='data'><BarGraph2 data={this.state.expense} /></Col>
-    </div>
-  </Row>
-  </div>
-  <div className='space-1'></div>
-<div>
- <Row>
- <Col className='wrapper-1'>
-   <h2 className='line'> <span className='text2'>Timeline</span></h2>
-   <Time data={this.state.expense.filter(x => parseFloat(x.amount) !== 0)}/>
-   </Col>
-   <Spacer amount={15} />
-  <Col sm={7} className='wrapper-1'>
-  <h2 className='line'> <span className='text2'>Recent Transactions</span></h2>
-  <Trans data={this.state.expense.filter(x => parseFloat(x.amount) !== 0)}/>
-  </Col>
-   
- </Row>
+                                  /> 
+            </CardHeader>
+            <CardBody>
+           
+              <p > <ArrowUpward  />Tracking of Company's Progress</p>
+            </CardBody>
+            <CardFooter chart>
+              <div >
+                <AccessTime />  according to filtered dates
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader >
+            <BarGraph2 data={this.state.expense} />
+            </CardHeader>
+            <CardBody>
+              <p >Analysis of Company's Revenues</p>
+            </CardBody>
+            <CardFooter chart>
+              <div >
+                <AccessTime /> according to filtered dates
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+
+
 </div>
 
    </>

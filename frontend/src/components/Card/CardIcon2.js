cdbdb7cf4126a82b0e7e4eef2cbc4d8a0f4c985b
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:31:36 +0800

    Add files via upload

---
+++ frontend/src/components/Card/CardIcon2.js
@@ -0,0 +1,41 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+
+// core components
+import cardIconStyle2 from "../assets/jss/material-dashboard-react/components/CardIconStyle2";
+
+const useStyles = makeStyles(cardIconStyle2);
+
+export default function CardIcon2(props) {
+  const classes = useStyles();
+  const { className, children, color, ...rest } = props;
+  const cardIconClasses = classNames({
+    [classes.cardIcon]: true,
+    [classes[color + "CardHeader"]]: color,
+    [className]: className !== undefined
+  });
+  return (
+    <div className={cardIconClasses} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+CardIcon2.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.oneOf([
+    "warning",
+    "success",
+    "danger",
+    "info",
+    "primary",
+    "rose"
+  ]),
+  children: PropTypes.node
+};

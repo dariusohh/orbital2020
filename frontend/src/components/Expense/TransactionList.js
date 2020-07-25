d242f979d82bd91b4e550161d7174c09930bfb75
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:06:52 +0800

    Update TransactionList.js
    Material _ui

--- frontend/src/components/Expense/TransactionList.js
+++ frontend/src/components/Expense/TransactionList.js
@@ -3,33 +3,49 @@ import axios from 'axios';
 import {Expense} from './Expense';
 import {Revenue} from './Revenue';
 import AddButton from './AddButton';
-
+import { Row,Col}from 'react-grid-system';
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
 class TransactionList extends React.Component {
 
     render() {
     return (
+      <>
+      <div className="box">
+        <Row>
+          <Col>
+                <h3>REVENUE</h3>
+                </Col>
+                <Col>
+             <h3>EXPENSE</h3>
+             </Col>
+             </Row>
+
+      </div>
+    
         <div className = "trans-container">
                 <ul className="list">
-                  <h3>Revenue</h3>
+                
                   {this.props.data.map(transaction => (
                   <Revenue key={transaction.id} transaction={transaction} />
                   ))}
+                  <br/>
                   <AddButton type="Revenue"/>
                 </ul>
              
           <div className = "divider"></div>
            
                 <ul className="list">
-                <h3>Expense</h3>
+                
                 {this.props.data.map(transaction => (
                   <Expense key={transaction.id} transaction={transaction} />
                   ))}
+                  <br/>
                   <AddButton type="Expense"/>
+                
                 </ul>
         </div>
-
+</>
     )
   }
 }

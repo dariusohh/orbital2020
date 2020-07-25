c76ee70e549448e04dd2af233ce84898fb0fe882
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:04:54 +0800

    Update IncomeExpenses.js
    Material-UI

--- frontend/src/components/Expense/IncomeExpenses.js
+++ frontend/src/components/Expense/IncomeExpenses.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import { Row,Col}from 'react-grid-system';
+import './import.css';
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
@@ -7,35 +9,60 @@ class IncomeExpenses extends React.Component {
 
   render() {
   return (
-        <div className="inc-exp-container">
-        <div>
-          <h4>Revenue</h4>
-    <p className="money plus">{this.props.data
+    <>
+    <div className ="box-3">
+    <Row> 
+      <Col>
+      <div>
+      <br/>
+          <br/>
+          <br/>
+      <h4 style={{ textAlign:'center' , color :'DimGrey'}}>Revenue</h4>
+        
+    <p className="money plus"> $ {this.props.data
       .map(transaction => transaction.amount).map(num => parseFloat(num))
       .filter(item => item > 0)
       .reduce((acc, item) => (acc += item), 0)
       .toFixed(2)}
       </p>
         </div>
-        <div>
-          <h4>Nett Profit</h4>
-          <p className="money total">{this.props.data
-          .map(transaction => transaction.amount)
-          .map(num => parseFloat(num))
-          .reduce((acc, item) => (acc += item), 0).toFixed(2)}
-          </p>
-        </div>
-        <div>
-          <h4>Expense</h4>
-          <p className="money minus">{(this.props.data
+      </Col>
+      <Col>
+      <div>
+      <br/>
+          <br/>
+          <br/>
+       <h4 style={{ textAlign:'center',color :'DimGrey' }}>Expense</h4>
+         
+          <p className="money minus"> $ {(this.props.data
           .map(transaction => transaction.amount)
           .map(num => parseFloat(num))
           .filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1)
           .toFixed(2)}</p>
         </div>
-        
+      </Col>
+      <Col>
+      <div >
+      <br/>
+          <br/>
+          <br/>
+   
+          <h4 style={{ textAlign:'center' ,color :'DimGrey'}}>Nett Profit</h4>
+          <p className="money total">
+            $ {this.props.data
+          .map(transaction => transaction.amount)
+          .map(num => parseFloat(num))
+          .reduce((acc, item) => (acc += item), 0).toFixed(2)}
+          </p>
+       
+        </div>
+      </Col>
 
-      </div>
+    </Row>
+    </div>
+
+  
+      </>
     )
   }
 }

a48ff2b0dab05d28075746672aa0e2a1091ed132
mayvechua <65010120+mayvechua@users.noreply.github.com>
2020-07-25 18:03:16 +0800

    Update AddButton.js
    Material-UI

--- frontend/src/components/Expense/AddButton.js
+++ frontend/src/components/Expense/AddButton.js
@@ -1,60 +1,149 @@
 import React from 'react';
 import axios from 'axios';
 import Popup from "reactjs-popup";
-
+import Fab from '@material-ui/core/Fab';
+import { Add as AddIcon } from '@material-ui/icons';
+import Button from '@material-ui/core/button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-
 class AddButton extends React.Component {
-
-    onSubmit = (event) => {
-      const name = event.target.name.value;
-      let amount = event.target.amount.value;
+    state = {
+      category: "",
+      amount: '',
+    }
+    onSubmit =()=> {
+      const name = this.state.category;
+      let amount = this.state.amount;
       if (this.props.type === "Expense") {
-        amount = -amount
+        amount = -this.state.amount
       }
       const username = localStorage.getItem("username");
       return axios.post('api/', {name: name, username: username,
               amount: amount})
               .catch(err => console.log(err))
       }
+      
+      handleChange = (event) => {
+        this.setState ({
+          category: event.target.value
+        })
 
+      };
+    
+      handleamount = (event) => {
+        this.setState ({
+          amount: event.target.value
+        })
+
+      };
       render() {
+        const categories = [
+          {
+            value: 'Entertainment',
+            label: 'Entertainment'
+          },
+          {
+            value: 'Equipment & Furniture',
+            label: 'Equipment & Furniture'
+          },
+          {
+            value: "Marketing",
+            label: "Marketing"
+          },
+          {
+            value: "Office Supplies",
+            label: "Office Supplies"
+          },
+          {
+            value: "Payroll",
+            label: "Payroll"
+          },
+          {
+            value: "Rent",
+            label: "Rent"
+          },
+          {
+            value: "Software",
+            label: "Software"
+          },
+          {
+            value: "Taxes",
+            label: "Taxes"
+          },
+          {
+            value: "Travel",
+            label: "Travel"
+          },
+          {
+            value: "Utilities",
+            label: "Utilities"
+          },
+          {
+            value: "Others",
+            label:"Others"
+          }
+  
+        ]
     return (
-        <Popup trigger={<button className = "add-btn">Add +</button>} modal closeOnDocumentClick>
-        <div>
-            <h3>Add {this.props.type}</h3>
-        <form onSubmit={this.onSubmit}>
-            <div className ="form-control">
-            <label htmlFor="text">Category</label>
-            <br/>
-            { this.props.type === "Revenue" ?
-            <input type="text" style={{margin:"0"}} name = "name" placeholder="Enter..." />
-            :
-            <select className="exp-dropdown" name = "name">
-              <option value="Entertainment">Entertainment</option>
-              <option value="Equipment & Furniture">Equipment & Furniture</option>
-              <option value="Marketing">Marketing</option>
-              <option value="Office Supplies">Office Supplies</option>
-              <option value="Payroll">Payroll</option>
-              <option value="Rent">Rent</option>
-              <option value="Software">Software</option>
-              <option value="Taxes">Taxes</option>
-              <option value="Travel">Travel</option>
-              <option value="Utilities">Utilities</option>
-              <option value="Others">Others</option>
-            </select>
-            }
-            </div>
-            <div className="form-control">
-            <label htmlFor="amount"> Amount <br />
-                </label>
-            <input name= "amount" type="number" step = "0.01"
-            placeholder="Enter amount..." />
-            </div>
-            <button className="btn">Add {this.props.type}</button>
+        <Popup trigger={ <Fab color="primary" aria-label="add">
+        <AddIcon /> 
+        </Fab>} modal closeOnDocumentClick>
+<div>
+        <form  noValidate autoComplete="off">
+        <InputLabel >Category</InputLabel>
+        {this.props.type === "Expense" &&
+        <TextField
+        fullWidth
+          id="name"
+          select
+          label="Select"
+          value={this.state.category}
+          onChange={this.handleChange}
+          helperText="Please select your category"
+        >
+          {categories.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      } 
+      {this.props.type === "Revenue" &&
+        <TextField
+        fullWidth
+        
+        label="Enter"
+        placeholder="Please enter your category"
+        value={this.state.category}
+          onChange={this.handleChange}
+       
+      />
+      
+      }
+          <br/>
+        <br/>
+        <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+          <Input
+             fullWidth
+            id="standard-adornment-amount"
+            value={this.state.amount}
+            onChange={this.handleamount}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          />
+     <br/>
+     <br/>
+        <Button variant="contained" fullWidth onClick = {this.onSubmit} type="submit" color="primary">
+          Add {this.props.type}
+          </Button>
+
         </form>
         </div>
     </Popup>
+
     )
     }
   }
